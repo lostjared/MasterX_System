bf5781da00110fb82226e4e6e75afc5eda833c8c
Jared Bruni <jaredbruni@gmail.com>
2024-10-07 08:40:53 -0700

    seek
    seek

--- compiler/ETL/libetl/io.c
+++ compiler/ETL/libetl/io.c
@@ -17,6 +17,10 @@ long file_read(void *f, void *data, long size) {
     return fread(data, size, 1, (FILE*)f);
 }
 
+long file_seek(void *f, long offset, long origin) {
+    return fseek((FILE*)f, offset, (int)origin);
+}
+
 void file_close(void *f) {
     fclose((FILE*)f);
 }
--- compiler/ETL/libetl/io.e
+++ compiler/ETL/libetl/io.e
@@ -1,10 +1,9 @@
 #ifndef _IO_H_
 #define _IO_H_
-
 define @file_open($src, $op);
 define file_print(@file, $data);
 define file_write(@file, @data, size);
 define file_read(@file, @data, size);
+define file_seek(@file, offset, origin);
 define file_close(@file);
-
 #endif
