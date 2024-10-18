7d96722a9957e5ab3ffa9d5d2cb9c9ea39b6e3f6
Jared Bruni <jaredbruni@gmail.com>
2024-10-17 20:04:52 -0700

    sdl
    sdl

--- compiler/ETL/libetl/shared/sdl/sdl.cpp
+++ compiler/ETL/libetl/shared/sdl/sdl.cpp
@@ -68,7 +68,7 @@ typedef void  (*addFunction)(const char *src, void *ptr);
 extern "C" void sdl_init();
 extern "C" void sdl_init_size(long fs);
 extern "C" void sdl_quit();
-extern "C" void sdl_pump();
+extern "C" long sdl_pump();
 extern "C" void sdl_release();
 extern "C" void sdl_create(const char *name, long  tw, long th, long w, long h);
 extern "C" void sdl_setcolor(long r, long g, long b, long a);
@@ -97,6 +97,7 @@ extern "C" void sdl_draw_gradient(long x, long y, long w, long h);
 
 
 interp::Var func_sdl_init(const std::vector<interp::Var> &v) {
+    check_args("sdl_init", v, {});
     sdl_init();
     return interp::Var("return", (long)0);
 }
@@ -108,19 +109,160 @@ interp::Var func_sdl_init_size(const std::vector<interp::Var> &v) {
 }
 
 interp::Var func_sdl_quit(const std::vector<interp::Var> &v) {
+    check_args("sdl_quit", v, {});
     sdl_quit();
     return interp::Var("return", (long)0);
 }
 
 interp::Var func_sdl_pump(const std::vector<interp::Var> &v) {
-    sdl_pump();
-    return interp::Var("return", (long)0);
+    check_args("sdl_pump", v, {});
+    return interp::Var("return", (long)sdl_pump());
 }
 
 interp::Var func_sdl_release(const std::vector<interp::Var> &v) {
+    check_args("sdl_release", v, {});
     sdl_release();
     return interp::Var("return", (long)0);
 }
+
+interp::Var func_sdl_create(const std::vector<interp::Var> &v) {
+    check_args("sdl_create", v, {ast::VarType::STRING, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_create(v.at(0).string_value.c_str(), v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value, v.at(4).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_setcolor(const std::vector<interp::Var> &v) {
+    check_args("sdl_setcolor", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_setcolor(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_fillrect(const std::vector<interp::Var> &v) {
+    check_args("sdl_fillrect", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_fillrect(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_flip(const std::vector<interp::Var> &v) {
+    check_args("sdl_flip", v, {});
+    sdl_flip();
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_clear(const std::vector<interp::Var> &v) {
+    check_args("sdl_clear", v, {});
+    sdl_clear();
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_keydown(const std::vector<interp::Var> &v) {
+    check_args("sdl_keydown", v, {ast::VarType::NUMBER});
+    return interp::Var("return", (long)sdl_keydown(v.at(0).numeric_value));
+}
+
+interp::Var func_sdl_getticks(const std::vector<interp::Var> &v) {
+    check_args("sdl_getticks", v, {});
+    return interp::Var("return", (long)sdl_getticks());
+}
+
+interp::Var func_sdl_loadtex(const std::vector<interp::Var> &v) {
+    check_args("sdl_loadtex", v, {ast::VarType::STRING});
+    return interp::Var("return", (void*)sdl_loadtex(v.at(0).string_value.c_str()));
+}
+
+interp::Var func_sdl_destroytex(const std::vector<interp::Var> &v) {
+    check_args("sdl_destroytex", v, {ast::VarType::POINTER});
+    sdl_destroytex((SDL_Texture*)v.at(0).ptr_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_copytex(const std::vector<interp::Var> &v) {
+    check_args("sdl_copytex", v, {ast::VarType::POINTER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_copytex((SDL_Texture*)v.at(0).ptr_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value, v.at(4).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_settarget(const std::vector<interp::Var> &v) {
+    check_args("sdl_settarget", v, {ast::VarType::POINTER});
+    sdl_settarget((SDL_Texture*)v.at(0).ptr_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_cleartarget(const std::vector<interp::Var> &v) {
+    check_args("sdl_cleartarget", v, {});
+    sdl_cleartarget();
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_printtext(const std::vector<interp::Var> &v) {
+    check_args("sdl_printtext", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::STRING});
+    sdl_printtext(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).string_value.c_str());
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_settextcolor(const std::vector<interp::Var> &v) {
+    check_args("sdl_settextcolor", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_settextcolor(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_drawline(const std::vector<interp::Var> &v) {
+    check_args("sdl_drawline", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_drawline(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_drawcircle(const std::vector<interp::Var> &v) {
+    check_args("sdl_drawcircle", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_drawcircle(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_mousedown(const std::vector<interp::Var> &v) {
+    check_args("sdl_mousedown", v, {});
+    return interp::Var("return", (long)sdl_mousedown());
+}
+
+interp::Var func_sdl_mousex(const std::vector<interp::Var> &v) {
+    check_args("sdl_mousex", v, {});
+    return interp::Var("return", (long)sdl_mousex());
+}
+
+interp::Var func_sdl_mousey(const std::vector<interp::Var> &v) {
+    check_args("sdl_mousey", v, {});
+    return interp::Var("return", (long)sdl_mousey());
+}
+
+interp::Var func_sdl_messagebox(const std::vector<interp::Var> &v) {
+    check_args("sdl_messagebox", v, {ast::VarType::STRING});
+    sdl_messagebox(v.at(0).string_value.c_str());
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_delay(const std::vector<interp::Var> &v) {
+    check_args("sdl_delay", v, {ast::VarType::NUMBER});
+    sdl_delay(v.at(0).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_setstartcolor(const std::vector<interp::Var> &v) {
+    check_args("sdl_setstartcolor", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_setstartcolor(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_setendcolor(const std::vector<interp::Var> &v) {
+    check_args("sdl_setendcolor", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_setendcolor(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
+interp::Var func_sdl_draw_gradient(const std::vector<interp::Var> &v) {
+    check_args("sdl_draw_gradient", v, {ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER, ast::VarType::NUMBER});
+    sdl_draw_gradient(v.at(0).numeric_value, v.at(1).numeric_value, v.at(2).numeric_value, v.at(3).numeric_value);
+    return interp::Var("return", (long)0);
+}
+
 
 extern "C" void initTable(addFunction func) {
     func("sdl_init", (void*)func_sdl_init);
@@ -128,4 +270,28 @@ extern "C" void initTable(addFunction func) {
     func("sdl_quit", (void*)func_sdl_quit);
     func("sdl_pump", (void*)func_sdl_pump);
     func("sdl_release", (void*)func_sdl_release);
+    func("sdl_create", (void*)func_sdl_create);
+    func("sdl_setcolor", (void*)func_sdl_setcolor);
+    func("sdl_fillrect", (void*)func_sdl_fillrect);
+    func("sdl_flip", (void*)func_sdl_flip);
+    func("sdl_clear", (void*)func_sdl_clear);
+    func("sdl_keydown", (void*)func_sdl_keydown);
+    func("sdl_getticks", (void*)func_sdl_getticks);
+    func("sdl_loadtex", (void*)func_sdl_loadtex);
+    func("sdl_destroytex", (void*)func_sdl_destroytex);
+    func("sdl_copytex", (void*)func_sdl_copytex);
+    func("sdl_settarget", (void*)func_sdl_settarget);
+    func("sdl_cleartarget", (void*)func_sdl_cleartarget);
+    func("sdl_printtext", (void*)func_sdl_printtext);
+    func("sdl_settextcolor", (void*)func_sdl_settextcolor);
+    func("sdl_drawline", (void*)func_sdl_drawline);
+    func("sdl_drawcircle", (void*)func_sdl_drawcircle);
+    func("sdl_mousedown", (void*)func_sdl_mousedown);
+    func("sdl_mousex", (void*)func_sdl_mousex);
+    func("sdl_mousey", (void*)func_sdl_mousey);
+    func("sdl_messagebox", (void*)func_sdl_messagebox);
+    func("sdl_delay", (void*)func_sdl_delay);
+    func("sdl_setstartcolor", (void*)func_sdl_setstartcolor);
+    func("sdl_setendcolor", (void*)func_sdl_setendcolor);
+    func("sdl_draw_gradient", (void*)func_sdl_draw_gradient);
 }
