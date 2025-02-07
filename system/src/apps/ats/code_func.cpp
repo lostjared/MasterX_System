f0740f288086ed0be8b9ba8c65df5d2da6d4f919
Jared Bruni <jaredbruni@gmail.com>
2025-02-06 17:17:57 -0800

    update code insert
    updated code insert

--- system/src/apps/ats/code_func.cpp
+++ system/src/apps/ats/code_func.cpp
@@ -7,7 +7,7 @@ namespace interp {
         file.open(text, std::ios::out);
         if(!file.is_open()) return false;
         for(unsigned int i = 0; i < lines.size(); ++i) {
-            file << lines[i].index << " " << lines[i].text << "\r\n";
+            file << lines[i].index << " " << lines[i].text << "\n";
         }
         file.close();
         return true;
@@ -54,12 +54,15 @@ namespace interp {
     }
     
     void insertText(std::vector<lex::Token> &tokens, const TextLine &in) {
+        
         for(unsigned int i = 0; i < lines.size(); ++i) {
             if(lines[i].index == in.index) {
                 if(checkInstruction(tokens, in)) {
                 	lines[i].text = in.text;
                 	return;
-                } else return;
+                } else {
+                    return;
+                }
             }
         }
         if(checkInstruction(tokens, in)) {
@@ -72,7 +75,7 @@ namespace interp {
         if(tokens.size()>=2 && input_line.length()>0) {
             lex::Token_type num = tokens[0].getTokenType();
             if(num != lex::TOKEN_DIGIT) {
-                stream << "Requires line number before code.\r\n";
+                std::cerr << "Requires line number before code.\n";
                 return;
             }
             int value = atoi(tokens[0].getToken().c_str());
@@ -80,8 +83,17 @@ namespace interp {
             codetext = input_line.substr(input_line.find(tokens[0].getToken())+tokens[0].getToken().length()+1, input_line.length());
             TextLine in(value, codetext);
             insertText(tokens, in);
-        } else {
-            stream << "Error invalid input.\r\n";
+        } else if(tokens.size()==1 && input_line.length()>0) {
+            int index_n = atoi(tokens[0].getToken().c_str());
+            for(int i = 0; i < lines.size(); ++i) {
+                if(lines[i].index == index_n) {
+                    lines.erase(lines.begin()+i);
+                    return;
+                }
+            }
+        } 
+        else {
+            std::cerr << "Error invalid input.\n";
             return;
         }
     }
@@ -90,7 +102,7 @@ namespace interp {
     // will do for translating
     bool checkInstruction(std::vector<lex::Token> &tokens, const TextLine &text) {
         if(tokens.size()<= 1) {
-            stream << "Error: Statement requires instruction.\r\n";
+            std::cerr << "Error: Statement requires instruction.\n";
             return false;
         }
         static unsigned int inc_offset = 0;
@@ -98,13 +110,13 @@ namespace interp {
             icode::opc op = icode::strtoInc(tokens[1].getToken());
             if(op == icode::opc::NOTINC) {
                 if(tokens[1].getTokenType() != lex::TOKEN_CHAR) {
-                    stream << "Syntax Error: After line number requires either Label or instruction.\r\n";
+                    std::cerr << "Syntax Error: After line number requires either Label or instruction.\n";
                     return false;
                 }
                 inc_offset = 1;
                 op = icode::strtoInc(tokens[1+inc_offset].getToken());
                 if(op == icode::opc::NOTINC) {
-                    stream << "Syntax Error: After Label requires Instruction.\r\n";
+                    std::cerr << "Syntax Error: After Label requires Instruction.\n";
                     return false;
                 }
             } else {
@@ -114,11 +126,11 @@ namespace interp {
                         if(tokens.size()>=4+inc_offset) {
                             if(tokens[3+inc_offset].getTokenType() == lex::TOKEN_DIGIT) {}
                             else if(tokens[3+inc_offset].getTokenType() != lex::TOKEN_DIGIT && tokens[3+inc_offset].getTokenType() != lex::TOKEN_HEX) {
-                                stream << "Syntax Error: requires either digit or $.\r\n";
+                                std::cerr << "Syntax Error: requires either digit or $.\n";
                                 return false;
                                 
                             } else if(tokens[3+inc_offset].getTokenType() != lex::TOKEN_HEX) {
-                                stream << "Syntax Error: $ is followed by a Hex value\r\n";
+                                std::cerr << "Syntax Error: $ is followed by a Hex value\n";
                                 return false;
                             }
                             
@@ -127,12 +139,12 @@ namespace interp {
                                     // check if register
                                 }
                             } else if(tokens.size()>=5+inc_offset && tokens[4+inc_offset].getToken() != ",") {
-                                stream << "Syntax Error: Expecting comma instead I found: " << tokens[4].getToken() << "\r\n";
+                                std::cerr << "Syntax Error: Expecting comma instead I found: " << tokens[4].getToken() << "\n";
                                 return false;
                             }
                             
                         } else {
-                            stream << "Syntax Error: Missing value after #\r\n";
+                            std::cerr << "Syntax Error: Missing value after #\n";
                             return false;
                         }
                     }
@@ -157,11 +169,11 @@ namespace interp {
             
         }
         // for now output each token
-        stream << "Code Line: ["<<text.index <<"] - [" << text.text << "] = {\r\n";
+        std::cout << "Code Line: ["<<text.index <<"] - [" << text.text << "] = {\n";
         for(unsigned int i = 0; i < tokens.size(); ++i) {
-            stream << tokens[i] << "\r\n";
+            std::cout << tokens[i] << "\n";
         }
-        stream << "}\r\n\r\n";
+        std::cout << "}\n\n";
         return true;
     }
 }
